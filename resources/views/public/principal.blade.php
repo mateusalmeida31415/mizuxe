d41a6ea3301f26d5ff463c4f015d1d8aafe80468
mateusalmeida31415 <mateusalmeida31415@gmail.com>
2021-11-06 09:02:20 -0300

    Finalizaçao da Authors Section

--- resources/views/public/principal.blade.php
+++ resources/views/public/principal.blade.php
@@ -32,4 +32,10 @@
     <why-component></why-component>
 
     {{-- Authors Section --}}
+    <authors-component :authors="[
+        {'name': 'Susan Williams', 'image': '/img/person1.jpg', 'function': 'Lead Writer', 'feedback': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus incidunt dolorum beatae, inventore porro quaerat.'},
+        {'name': 'Grace Smith', 'image': '/img/person2.jpg', 'function': 'Co-Writer', 'feedback': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus incidunt dolorum beatae, inventore porro quaerat.'},
+        {'name': 'John Doe', 'image': '/img/person3.jpg', 'function': 'Editor', 'feedback': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus incidunt dolorum beatae, inventore porro quaerat.'},
+        {'name': 'Kevin Swanson', 'image': '/img/person4.jpg', 'function': 'Designer', 'feedback': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus incidunt dolorum beatae, inventore porro quaerat.'}
+    ]"></authors-component>
 @endsection
