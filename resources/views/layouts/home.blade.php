31234dbe349eeb28bdfa4e8c4cd7d3aea27cf5fd
mateusalmeida31415 <mateusalmeida31415@gmail.com>
2021-11-08 08:42:14 -0300

    Melhorando a exibiçao da barra de navegaçao

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -25,9 +25,6 @@
     {{-- Jquery 3.5.1 --}}
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 
-    {{-- Boostrap 4.6.1 --}}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
-
     {{-- Vue.js --}}
     <script src="{{asset('js/app.js')}}"></script>
 
