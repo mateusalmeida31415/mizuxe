f29784c2385b26fbb0663cc219ce42cb7f1f5189
mateusalmeida31415 <mateusalmeida31415@gmail.com>
2021-11-09 09:43:19 -0300

    Box refactoring

--- resources/js/components/Box.vue
+++ resources/js/components/Box.vue
@@ -1,11 +1,14 @@
 <template>
+    <!-- card: Objeto com propriedades do card. title(Título do card), 
+    text(Texto explicativo do card) -->
     <section id="boxes" class="py-5">
         <div class="container">
             <div class="row">
                 <!-- Usar um array cuja a qualtidade seja multipla de 12 -->
                 <template v-for="(value, index) in card">
-                    <!-- Quando o final do array for atingido não será adicionando uma margin-botton -->
-                    <div :class="'col-md-3 ' + (((card.length - 1) != index) ? 'mb-3' : '')">
+                    <!-- Quando o final do array for atingido não será adicionando o margin-botton -->
+                    <!-- O layout irá se adaptar de acordo com a quantidade de elementos do array -->
+                    <div :class="'col-md-' + (12/(card.length)) + (((card.length - 1) != index) ? ' mb-3' : '')">
                         <div class="card border border-primary text-center">
                             <!-- Variação das cores dos cards -->
                             <div :class="'card-body' + ((index % 2) ? ' text-primary' : ' bg-primary text-white')">
@@ -22,6 +25,6 @@
 
 <script>
     export default {
-        props: ['qtd', 'card']
+        props: ['card']
     }
 </script>
