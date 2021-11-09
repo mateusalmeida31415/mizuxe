2a5bf103d20d2f34f1d85bcd498ee618ef388668
mateusalmeida31415 <mateusalmeida31415@gmail.com>
2021-11-09 11:10:27 -0300

    Authors section refactoring

--- resources/views/public/principal.blade.php
+++ resources/views/public/principal.blade.php
@@ -47,7 +47,11 @@
     <why-component id="why"></why-component>
 
     {{-- Authors Section --}}
-    <authors-component id="authors" :authors="[
+    <authors-component 
+    id="authors"
+    title="Meet The Authors" 
+    text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione nihil reprehenderit corporis in at consequuntur?"
+    :authors="[
         {'name': 'Susan Williams', 'image': '/img/person1.jpg', 'function': 'Lead Writer', 'feedback': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus incidunt dolorum beatae, inventore porro quaerat.'},
         {'name': 'Grace Smith', 'image': '/img/person2.jpg', 'function': 'Co-Writer', 'feedback': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus incidunt dolorum beatae, inventore porro quaerat.'},
         {'name': 'John Doe', 'image': '/img/person3.jpg', 'function': 'Editor', 'feedback': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus incidunt dolorum beatae, inventore porro quaerat.'},
